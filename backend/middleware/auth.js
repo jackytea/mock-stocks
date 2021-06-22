1d42cd6480124580338b14463c29fd13236cdccf
Jacky Tea <teajacks@outlook.com>
2021-06-22 11:32:52 -0400

    auth checks

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization?.split(" ")[1];
     const isCustomAuth = token.length < 500;
     let contentDecoded;
 
@@ -17,10 +17,9 @@ const auth = async (req, res, next) => {
       contentDecoded = jwt.decode(token);
       req.userId = contentDecoded?.sub;
     }    
-
     next();
   } catch (error) {
-    console.log(error);
+    res.status(403).json({ message: "Not authenticated!" });
   }
 };
 
