843d6007bf37a1498ff5dbc654c36329ff8fdd9a
Jacky Tea <teajacks@outlook.com>
2021-07-08 14:30:51 -0400

    fix footer link

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
           <div class="w-full -mx-6 lg:w-2/5">
             <div class="px-6">
               <div>
-                <a href="#" class="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Mock Stocks</a>
+                <Link to="/" class="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Mock Stocks</Link>
               </div>
 
               <p class="max-w-md mt-2 text-gray-500 dark:text-gray-400">The investment platform for all.</p>
