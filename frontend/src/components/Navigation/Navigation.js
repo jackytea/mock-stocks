efe940e09242fd56369a3017c1813611e9a1fd16
Jacky Tea <teajacks@outlook.com>
2021-07-07 21:16:22 -0400

    more nav bar links

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -51,9 +51,12 @@ const Navigation = () => {
 					</div>
 
 					<div className="flex-1 md:flex md:items-center md:justify-between">
+
 						<div className="flex flex-col -mx-4 md:flex-row md:items-center md:mx-8">
+							<Link to="/" className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Home</Link>
 							<Link to="/markets" className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Markets</Link>
 							{user?.result && <Link to="/purchased" className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">Investments</Link>}
+							<Link to="/about" className="px-2 py-1 mx-2 mt-2 text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700">About</Link>
 						</div>
 
 						<div className="flex items-center mt-4 md:mt-0">
