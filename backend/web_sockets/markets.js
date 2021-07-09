1402db5c368ed006d5cab0017c657fefd88768e0
Jacky Tea <teajacks@outlook.com>
2021-07-09 17:19:44 -0400

    web socket error handling

--- backend/web_sockets/markets.js
+++ backend/web_sockets/markets.js
@@ -5,22 +5,27 @@ const delay = async (ms) => {
 }
 
 const stockPrice = async (socket, currPrice, delayMs, stockTicker, fluctuationRange, stockId) => {
-  let price = currPrice;
-  while (socket.connected) {
-    let up = Math.round(Math.random());
-    if (up) {
-      price += Math.random() * fluctuationRange;
-      up = false;
-    } else {
-      price -= Math.random() * fluctuationRange;
-      up = true;
+  try {
+    let price = currPrice;
+    while (socket.connected) {
+      let up = Math.round(Math.random());
+      if (up) {
+        price += Math.random() * fluctuationRange;
+        up = false;
+      } else {
+        price -= Math.random() * fluctuationRange;
+        up = true;
+      }
+      if (price < 0) {
+        price = 0;
+      }
+      await Stock.findOneAndUpdate({ id: stockId }, { currentPrice: price })
+      socket.emit(stockTicker, price.toFixed(2));
+      await delay(delayMs);
     }
-    if (price < 0) {
-      price = 0;
-    }
-    await Stock.findOneAndUpdate({ id: stockId }, { currentPrice: price })
-    socket.emit(stockTicker, price.toFixed(2));
-    await delay(delayMs);
+  } catch (error) {
+    socket.disconnect();
+    console.log("Market error", error);
   }
 }
 
