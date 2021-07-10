8c10bd93a1f9b85ebb2af7716dbce6c228346349
Jacky Tea <teajacks@outlook.com>
2021-07-10 12:43:06 -0400

    sort logs and transactions

--- backend/controllers/action_logs.js
+++ backend/controllers/action_logs.js
@@ -4,9 +4,9 @@ import ActionLog from '../models/action_log.js';
 const router = express.Router();
 
 // GET
-export const getActionLogs  = async (req, res) => {
+export const getActionLogs = async (req, res) => {
   try {
-    const allActionLogs = await ActionLog.find({ userId: req.userId });
+    const allActionLogs = await ActionLog.find({ userId: req.userId }).sort({ loggedAt: -1 });
     res.status(200).json(allActionLogs);
   } catch (error) {
     res.status(404).json({ message: "An error has occurred fetching your action logs." });
--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -4,9 +4,9 @@ import Transaction from '../models/transaction.js';
 const router = express.Router();
 
 // GET
-export const getTransactions  = async (req, res) => {
+export const getTransactions = async (req, res) => {
   try {
-    const allTransactions = await Transaction.find({ userId: req.userId });
+    const allTransactions = await Transaction.find({ userId: req.userId }).sort({ transactedAt: -1 });
     res.status(200).json(allTransactions);
   } catch (error) {
     res.status(404).json({ message: "An error has occurred fetching your transactions." });
