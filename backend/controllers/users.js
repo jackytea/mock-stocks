3bd74604879d2645b69cf74900ae04f6d2936cbd
Jacky Tea <teajacks@outlook.com>
2021-07-10 12:43:23 -0400

    user management routes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
 import User from '../models/user.js';
+import PurchasedStock from '../models/purchased_stock.js';
+import Transaction from '../models/transaction.js';
 import ActionLog from '../models/action_log.js';
 
 dotenv.config();
 const jwtSecret = process.env.JWT_SECRET;
+const router = express.Router();
 
 export const registerUser = async (req, res) => {
   try {
@@ -69,3 +74,78 @@ export const getUserInfo = async (req, res) => {
     res.status(404).json({ message: "An error has occurred fetching the user requested." });
   }
 };
+
+export const updateUserName = async (req, res) => {
+  try {
+    const { firstName, lastName } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(req.userId)) {
+      return res.status(404).send(`No user with id: ${req.userId}`);
+    }
+
+    if (req.userId === process.env.GUEST_ID) {
+      return res.status(400).send("Not allowed to modify guest account!");
+    }
+
+    await User.findByIdAndUpdate(req.userId, { name: `${firstName} ${lastName}` });
+    res.status(200).json({ message: "Username successfully updated!" });
+  } catch (error) {
+    res.status(404).json({ message: "An error has occurred updating your username." });
+  }
+};
+
+export const updateUserPassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword, newPasswordConfirmed } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(req.userId)) {
+      return res.status(404).send(`No user with id: ${req.userId}`);
+    }
+
+    if (req.userId === process.env.GUEST_ID) {
+      return res.status(400).send("Not allowed to modify guest account!");
+    }
+
+    if (newPassword !== newPasswordConfirmed) {
+      return res.status(400).json({ message: "Passwords do not match!." });
+    }
+
+    const userData = await User.findById(req.userId);
+    const passwordCorrect = await bcrypt.compare(currentPassword, userData.password);
+
+    if (!passwordCorrect) {
+      return res.status(400).json({ message: "Invalid old password!" });
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHashed = await bcrypt.hash(newPassword, salt);
+
+    await User.findByIdAndUpdate(req.userId, { password: passwordHashed });
+    res.status(200).json({ message: "Password successfully updated!" });
+  } catch (error) {
+    res.status(404).json({ message: "An error has occurred updating your username." });
+  }
+};
+
+export const removeUser = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.userId)) {
+      return res.status(404).send(`No user with id: ${req.userId}`);
+    }
+
+    if (req.userId === process.env.GUEST_ID) {
+      return res.status(400).send("Not allowed to modify guest account!");
+    }
+
+    await ActionLog.deleteMany({ userId: req.userId });
+    await Transaction.deleteMany({ userId: req.userId });
+    await PurchasedStock.deleteMany({ userId: req.userId });
+    await User.findByIdAndDelete(req.userId);
+
+    res.status(200).json({ message: "User successfully deleted!" });
+  } catch (error) {
+    res.status(404).json({ message: "An error has occurred removing the user." });
+  }
+}
+
+export default router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,10 +2,13 @@ import express from "express";
 const router = express.Router();
 
 import auth from "../middleware/auth.js";
-import { registerUser, loginUser, getUserInfo } from "../controllers/users.js";
+import { registerUser, loginUser, getUserInfo, updateUserName, updateUserPassword, removeUser } from "../controllers/users.js";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/userinfo", auth, getUserInfo);
+router.patch("/username", auth, updateUserName);
+router.patch("/password", auth, updateUserPassword);
+router.delete("/removeuser", auth, removeUser);
 
 export default router;
