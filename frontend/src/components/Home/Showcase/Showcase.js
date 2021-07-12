adfe0bdd59484de9b77df06f0e5a642c4b89bb1b
Jacky Tea <teajacks@outlook.com>
2021-07-11 20:19:42 -0400

    random featured stocks

--- frontend/src/components/Home/Showcase/Showcase.js
+++ frontend/src/components/Home/Showcase/Showcase.js
@@ -1,124 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { getStocks } from "../../../actions/stocks";
+import { MARKET_ERROR_OCCURRED } from '../../../constants/actions';
+import socketIOClient from "socket.io-client";
+import ShowcaseCardSkeleton from "./ShowcaseCardSkeleton";
+import ShowcaseCard from "./ShowcaseCard";
 
 const Showcase = () => {
+  const socket = socketIOClient(process.env.REACT_APP_STOCKS_API, { transports: ['websocket', 'polling', 'flashsocket'] });
+  const stocks = useSelector((state) => state.stocksReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getStocks());
+  }, [dispatch]);
+
+  useEffect(() => {
+    socket.connect();
+    dispatch({ type: MARKET_ERROR_OCCURRED, payload: "" });
+    return () => {
+      socket.disconnect();
+      dispatch({ type: MARKET_ERROR_OCCURRED, payload: "" });
+    }
+  }, [socket, dispatch]);
+
   return (
     <div className="bg-white dark:bg-gray-800">
-      <div className="container sm:px-32 py-16 sm:py-24 mx-auto">
-        <div className="flex flex-col items-center justify-center space-y-8 lg:-mx-4 lg:flex-row lg:items-stretch lg:space-y-0">
-          <div className="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
-            <div className="flex-shrink-0">
-              <h2 className="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-blue-400 uppercase rounded-lg bg-gray-50 dark:bg-gray-700">
-                Casual
-              </h2>
-            </div>
-            <div className="flex-shrink-0">
-              <span
-                className="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100"
-              >
-                Free
-              </span>
-            </div>
-            <ul className="flex-1 space-y-4">
-              <li className="text-gray-500 dark:text-gray-400">
-                Up to 5 projects
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                Up to 10 collaborators
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                2Gb of storage
-              </li>
-            </ul>
-
-            <button
-              className="inline-flex items-center justify-center px-4 py-2 font-semibold text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none"
-            >
-              Start free
-            </button>
-          </div>
-
-          <div className="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
-            <div className="flex-shrink-0">
-              <h2
-                className="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-blue-400 uppercase rounded-lg bg-gray-50 dark:bg-gray-700"
-              >
-                Profesional
-              </h2>
-            </div>
-            <div className="flex-shrink-0">
-              <span
-                className="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100"
-              >
-                $24.90
-              </span>
-              <span className="text-gray-500 dark:text-gray-400">
-                /month
-              </span>
-            </div>
-            <ul className="flex-1 space-y-4">
-              <li className="text-gray-500 dark:text-gray-400">
-                Up to 10 projects
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                Up to 20 collaborators
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                10Gb of storage
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                Real-time collaborations
-              </li>
-            </ul>
-
-            <button
-              className="inline-flex items-center justify-center px-4 py-2 font-semibold text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none"
-            >
-              Start free trial
-            </button>
-          </div>
-
-          <div className="flex flex-col w-full max-w-sm p-8 space-y-8 text-center bg-white border-2 border-gray-200 rounded-lg lg:mx-4 dark:bg-gray-800 dark:border-gray-700">
-            <div className="flex-shrink-0">
-              <h2
-                className="inline-flex items-center justify-center px-2 font-semibold tracking-tight text-blue-400 uppercase rounded-lg bg-gray-50 dark:bg-gray-700"
-              >
-                Expert
-              </h2>
-            </div>
-            <div className="flex-shrink-0">
-              <span
-                className="pt-2 text-4xl font-bold text-gray-800 uppercase dark:text-gray-100"
-              >
-                $49.90
-              </span>
-              <span className="text-gray-500 dark:text-gray-400">
-                /month
-              </span>
-            </div>
-            <ul className="flex-1 space-y-4">
-              <li className="text-gray-500 dark:text-gray-400">
-                Unlimited projects
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                Unlimited collaborators
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                Unlimited storage
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                Real-time collaborations
-              </li>
-              <li className="text-gray-500 dark:text-gray-400">
-                24x7 Support
-              </li>
-            </ul>
-
-            <button
-              className="inline-flex items-center justify-center px-4 py-2 font-semibold text-white uppercase transition-colors bg-blue-500 rounded-lg hover:bg-blue-700 focus:outline-none"
-            >
-              Start free trial
-            </button>
-          </div>
+      <div className="container sm:px-32 py-16 sm:py-16 mx-auto">
+        <h2 className="text-2xl text-center font-extrabold text-black dark:text-white sm:text-3xl mb-4 sm:mb-12">
+          <span className="block">
+            Today's Featured Picks
+          </span>
+        </h2>
+        <div className="flex flex-col items-center justify-center lg:flex-row">
+          {!stocks?.length ? <ShowcaseCardSkeleton /> : <ShowcaseCard socket={socket} stocks={stocks} stockOne={stocks[0]} stockTwo={stocks[16]} stockThree={stocks[1]} />}
         </div>
       </div>
     </div>
---
+++ frontend/src/components/Home/Showcase/ShowcaseCardSkeleton.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+const ShowcaseCardSkeleton = () => {
+  return (
+    <>
+      <div className="animate-pulse max-w-xs mx-auto overflow-hidden bg-gray-200 rounded-lg shadow-lg dark:bg-gray-900 mt-4 w-96">
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-32 bg-white dark:bg-gray-700 rounded">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-4 bg-white dark:bg-gray-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-4 bg-white dark:bg-gray-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="animate-pulse max-w-xs mx-auto overflow-hidden bg-gray-200 rounded-lg shadow-lg dark:bg-gray-900 mt-4 w-96">
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-32 bg-white dark:bg-gray-700 rounded">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-4 bg-white dark:bg-gray-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-4 bg-white dark:bg-gray-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="animate-pulse max-w-xs mx-auto overflow-hidden bg-gray-200 rounded-lg shadow-lg dark:bg-gray-900 mt-4 w-96">
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-32 bg-white dark:bg-gray-700 rounded">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-4 bg-white dark:bg-gray-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="animate-pulse flex space-x-4">
+          <div className="p-4 max-w-sm w-full mx-auto">
+            <div className="flex-1 space-y-4 py-1">
+              <div className="space-y-2">
+                <div className="h-4 bg-white dark:bg-gray-700 rounded"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ShowcaseCardSkeleton;
