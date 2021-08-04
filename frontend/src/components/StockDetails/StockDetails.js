c590ab3506383a6c9e672644515b983bdbe6eddc
Jacky Tea <teajacks@outlook.com>
2021-08-04 13:48:44 -0400

    back to stock details on purchase button, open company link in new tab

--- frontend/src/components/StockDetails/StockDetails.js
+++ frontend/src/components/StockDetails/StockDetails.js
@@ -78,7 +78,7 @@ const StockDetails = (props) => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
 
-                <a href={stock.siteURL} className="mx-3 text-lg font-semibold text-gray-800 dark:text-white hover:text-gray-200"><h1>Company Website</h1></a>
+                <a href={stock.siteURL} className="mx-3 text-lg font-semibold text-gray-800 dark:text-white hover:text-gray-200" target="_blank" rel="noopener noreferrer"><h1>Company Website</h1></a>
               </div>
 
               <div className="px-6 py-4">
