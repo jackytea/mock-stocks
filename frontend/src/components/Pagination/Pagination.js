d95177cee78b90eaf29e734537188713f9957f1f
Jacky Tea <teajacks@outlook.com>
2021-08-12 11:24:32 -0400

    pagination mobile flex

--- frontend/src/components/Pagination/Pagination.js
+++ frontend/src/components/Pagination/Pagination.js
@@ -9,9 +9,9 @@ const Pagination = (props) => {
   }
 
   return (
-    <div className="flex justify-center mt-3">
+    <div className="flex flex-wrap justify-center mt-3">
       {pageNumbers.map(number => (
-        <button key={number} onClick={() => paginate(number)} className={currentPage === number ? "cursor-pointer flex items-center px-4 py-2 mx-1 text-gray-200 transition-colors duration-200 transform bg-blue-500 rounded-md dark:bg-blue-700 dark:text-gray-200 hover:bg-blue-300 dark:hover:bg-blue-700 hover:text-black dark:hover:text-gray-700" : "cursor-pointer flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-200 transform bg-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 hover:text-black dark:hover:text-gray-200"}>
+        <button key={number} onClick={() => paginate(number)} className={currentPage === number ? "cursor-pointer flex items-center px-4 py-2 mx-1 text-gray-200 transition-colors duration-200 transform bg-blue-500 rounded-md dark:bg-blue-700 dark:text-gray-200 hover:bg-blue-300 dark:hover:bg-blue-700 hover:text-black dark:hover:text-gray-700 sm:mt-0 mt-2" : "cursor-pointer flex items-center px-4 py-2 mx-1 text-gray-700 transition-colors duration-200 transform bg-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700 hover:text-black dark:hover:text-gray-200 sm:mt-0 mt-2"}>
           {number}
         </button>
       ))}
